ae68600af2df17ac603886eee806f51baec36db5
thomashart17 <thomashart17@gmail.com>
2022-11-19 22:01:18 +0000

    push button functionality

--- push_button.py
+++ push_button.py
@@ -1,13 +1,22 @@
 import RPi.GPIO as GPIO
+import time
 
-def button_power_callback():
-    print("Power Button was pushed!")
+last_button_press = 0
 
-def button_feature_callback():
-    print("Feature Button was pushed!")
+def button_power_callback(pin):
+    global last_button_press
+    if time.time() - last_button_press >= 0.3:
+        last_button_press = time.time()
+        print("Power Button was pushed!")
+
+def button_feature_callback(pin):
+    global last_button_press
+    if time.time() - last_button_press >= 0.3:
+        last_button_press = time.time()
+        print("Feature Button was pushed!")
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 BUTTON_POWER = 10
 BUTTON_FEATURE = 9
@@ -17,8 +26,8 @@ GPIO.setup(BUTTON_POWER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BUTTON_FEATURE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #attach event to pin
-GPIO.add_event_detect(BUTTON_POWER, GPIO.RISING, callback=button_power_callback)
-GPIO.add_event_detect(BUTTON_FEATURE, GPIO.RISING, callback=button_feature_callback)
+GPIO.add_event_detect(BUTTON_POWER, GPIO.FALLING, callback=button_power_callback)
+GPIO.add_event_detect(BUTTON_FEATURE, GPIO.FALLING, callback=button_feature_callback)
 
 message = input("Press enter to quit\n\n")
 GPIO.cleanup()
