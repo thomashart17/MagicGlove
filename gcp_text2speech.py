f5c73dded990b70e9d137518f011cb8d96bcba3a
thomashart17 <thomashart17@gmail.com>
2022-11-19 22:51:10 +0000

    Added api key

--- gcp_text2speech.py
+++ gcp_text2speech.py
@@ -4,17 +4,17 @@ from google.cloud import texttospeech_v1
 
 def speech_to_text(phrase, audio_file_path):
     print("Running GCP...")
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"path to json"
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"/home/magicglove/MagicGlove/api-key.json"
 
     # Instantiates a client
     client = texttospeech_v1.TextToSpeechClient()
-    
+
     # Set the text input to be synthesized
     synthesis_input = texttospeech_v1.SynthesisInput(text=phrase)
 
 
     voice = texttospeech_v1.VoiceSelectionParams(
-        language_code="en-ca", 
+        language_code="en-ca",
         ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL,
     )
 
@@ -26,8 +26,8 @@ def speech_to_text(phrase, audio_file_path):
     # Perform the text-to-speech request on the text input with the selected
     # voice parameters and audio file type
     response = client.synthesize_speech(
-        input=synthesis_input, 
-        voice=voice, 
+        input=synthesis_input,
+        voice=voice,
         audio_config=audio_config
     )
 
@@ -36,4 +36,3 @@ def speech_to_text(phrase, audio_file_path):
         # Write the response to the output file.
         out.write(response.audio_content)
         print('Audio content written to file "audiofile.mp3"')
-    
