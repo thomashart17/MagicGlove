ed3d09d7ce72ebf82c63ccda98c184da0248bbbc
ellauppal <eauppal@uwaterloo.ca>
2022-11-20 07:45:58 -0500

    Test

---
+++ test_file.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+#GPIO.setmode(GPIO.BOARD)
+
+def light_intensity():
+    count = 0
+    ldr = 7 #ldr is connected with pin number 7
+    count = 0
+    delayt = 0.1
+
+    #Output on the pin for
+    GPIO.setup(ldr, GPIO.OUT)
+    GPIO.output(ldr, GPIO.LOW)
+    time.sleep(delayt)
+    GPIO.setup(ldr, GPIO.IN)
+
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(7, GPIO.IN)
+  
+    #Count until the pin goes high
+    while (GPIO.input(ldr) == GPIO.LOW):
+        count += 1
+
+    return count
+
+if __name__ == "__main__":
+    try:
+        # Main loop
+        while True:
+            print(light_intensity(7))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
